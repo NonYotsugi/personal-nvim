c8c18d0dd86d2568d41008a519ce02d96850d2da
Yotsugi <koukutou@outlook.com>
2022-06-11 14:18:11 +0800

    chore: 完成配置

--- init.lua
+++ init.lua
@@ -1,13 +1,18 @@
 -- 基础配置
 require("basic")
+
 -- 快捷键映射
 require("keybindings")
+
 -- Packer插件管理
 require("plugins")
+
 -- 主题设置
 require("colorscheme")
+
 -- 自动命令
 require("autocmds")
+
 -- 插件配置
 require("plugin-config.nvim-tree")
 require("plugin-config.bufferline")
@@ -22,13 +27,16 @@ require("plugin-config.surround")
 require("plugin-config.comment")
 require("plugin-config.nvim-autopairs")
 require("plugin-config.fidget")
+
 -- 内置LSP
 require("lsp.setup")
 require("lsp.cmp")
 require("lsp.ui")
+
 -- 格式化
 -- require("lsp.formatter")
 require("lsp.null-ls")
+
 -- DAP
 -- require("dap.vimspector")
 require("dap.nvim-dap")
